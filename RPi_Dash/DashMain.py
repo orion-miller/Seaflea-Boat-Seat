e5e0a3889b1603e7270921759b14eb25504a3eb0
orion-miller <33074666+orion-miller@users.noreply.github.com>
2023-05-16 21:38:23 -0400

    add entries for layout

--- RPi_Dash/DashMain.py
+++ RPi_Dash/DashMain.py
@@ -1,20 +1,41 @@
 import kivy
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 from kivy.config import Config
+from kivy.core.window import Window
 
 kivy.require('2.1.0') # replace with your current kivy version !
 
-Config.set('graphics', 'width', '320')
-Config.set('graphics', 'height', '240')
-Config.set('graphics', 'resizable', False)
+#Set window resolution and turn off resizing
+Window.size = (320, 240)
+Config.set('graphics', 'resizable', False) 
+Config.write()
+class MainComponents(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(MainComponents, self).__init__(**kwargs)
+        #Set widgets
+        self.orientation = 'vertical'
+        self.add_widget(Label(text='   TIME: 11:03 AM', font_size='20sp', halign='left'))        
+        self.add_widget(Label(text='  SPEED: 28 MPH', font_size='20sp'))
+        self.add_widget(Label(text='    RPM: 2550', font_size='20sp'))
+        self.add_widget(Label(text='HEADING: NNE', font_size='20sp'))
+        self.add_widget(Label(text=' LIGHTS: ON', font_size='20sp'))   
+
+                    
 
 class MyApp(App):
 
     def build(self):
+        #Set properties for window
         self.title = "Dash"
         self.icon = "Images//OM_Logo_32_32.ico"
-        return Label(text='BATTERY: 100%')
+
+        return MainComponents()
         
 if __name__ == '__main__':
     MyApp().run()
