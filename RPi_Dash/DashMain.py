358269d52e21b44b8d00263a0d43ac43c691f0f6
orion-miller <33074666+orion-miller@users.noreply.github.com>
2023-05-16 20:21:50 -0400

    set title and resolution

--- RPi_Dash/DashMain.py
+++ RPi_Dash/DashMain.py
@@ -1,14 +1,18 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.config import Config
 
 kivy.require('2.1.0') # replace with your current kivy version !
+
+Config.set('graphics', 'width', '320')
+Config.set('graphics', 'height', '240')
 
 class MyApp(App):
 
     def build(self):
-        return Label(text='Hello world')
-
-
+        self.title = "Dash"
+        return Label(text='BATTERY: 100%')
+        
 if __name__ == '__main__':
     MyApp().run()
