c6713c332cfd864bc78cf2a0143c4e90a9b31e7f
orion-miller <33074666+orion-miller@users.noreply.github.com>
2023-05-16 20:27:59 -0400

    set icon, turn off resizing

--- RPi_Dash/DashMain.py
+++ RPi_Dash/DashMain.py
@@ -7,11 +7,13 @@ kivy.require('2.1.0') # replace with your current kivy version !
 
 Config.set('graphics', 'width', '320')
 Config.set('graphics', 'height', '240')
+Config.set('graphics', 'resizable', False)
 
 class MyApp(App):
 
     def build(self):
         self.title = "Dash"
+        self.icon = "Images//OM_Logo_32_32.ico"
         return Label(text='BATTERY: 100%')
         
 if __name__ == '__main__':
